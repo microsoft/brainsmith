0acd3d94f7c182e200c9c114ab282fb7705b2cbc
auphelia <jakobapk@web.de>
2025-12-20 10:25:39 +0000

    Switch crop to updated finn-hlslib fct

--- brainsmith/kernels/crop/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
-# Import the main operator, backends, and inference transform for the Crop
-from .crop import Crop
-from .crop_hls import Crop_hls
-from .infer_crop_from_gather import InferCropFromGather
-
-__all__ = ["Crop", "Crop_hls", "InferCropFromGather"]
